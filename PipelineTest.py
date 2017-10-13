d5ba0ce0b15a599e522982fea14761ccb50668ba
Mason Hall <masonhall@gmail.com>
2017-10-13 02:48:09 -0500

    Fixed

--- PipelineTest.py
+++ PipelineTest.py
@@ -1,31 +1,3 @@
-#!/usr/bin/env python
-
-# from sklearn.datasets import fetch_20newsgroups_vectorized
-# from lightning.classification import CDClassifier
-
-# # Load News20 dataset from scikit-learn.
-# bunch = fetch_20newsgroups_vectorized(subset="all")
-# X = bunch.data
-# y = bunch.target
-
-# # Set classifier options.
-# clf = CDClassifier(penalty="l1/l2",
-#                    loss="squared_hinge",
-#                    multiclass=True,
-#                    max_iter=20,
-#                    alpha=1e-4,
-#                    C=1.0 / X.shape[0],
-#                    tol=1e-3)
-
-# # Train the model.
-# clf.fit(X, y)
-
-# # Accuracy
-# print(clf.score(X, y))
-
-# # Percentage of selected features
-# print(clf.n_nonzero(percentage=True))
-
 # Author: Olivier Grisel <olivier.grisel@ensta.org>
 #         Peter Prettenhofer <peter.prettenhofer@gmail.com>
 #         Mathieu Blondel <mathieu@mblondel.org>
